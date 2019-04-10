d008c5046f0ea2d12db8562742af53f697c0b77f
chrii <c.pintarich@gmail.com>
2019-04-10 13:41:06 +0200

    ignore test

--- config/app.php
+++ config/app.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'wl-web-key' => 'Test',
-    'wl-dev-key' => 'Test',
+    'wl-web-key' => 'Test again',
+    'wl-dev-key' => 'Test again',
     /*
     |--------------------------------------------------------------------------
     | Application Name
